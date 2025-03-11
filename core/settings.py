e325ed87ebbfce46c807c8e9561c9846e3a86ef6
rishabhdev2700 <rishavkumar2700@gmail.com>
2025-03-11 18:52:43 +0000

    updated styles and signin signup flow

--- core/settings.py
+++ core/settings.py
@@ -166,7 +166,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 LOGOUT_URL = "accounts:logout"
 LOGIN_URL = "accounts:sign_in"
 RAZORPAY_ID = env("RAZORPAY_ID")
